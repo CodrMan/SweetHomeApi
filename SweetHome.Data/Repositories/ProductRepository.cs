25b86bdb444d78c8f3114b0b83d0a1aa2ec54afd
CodrMan <ponomarenko.allex@gmail.com>
2017-03-01 23:15:36 +0200

    first app start

--- SweetHome.Data/Repositories/ProductRepository.cs
+++ SweetHome.Data/Repositories/ProductRepository.cs
@@ -1,8 +1,9 @@
 ﻿using SweetHome.Core.Entities;
+using SweetHome.Core.Interfaces;
 
 namespace SweetHome.Data.Repositories
 {
-    public class ProductRepository : RepositoryBase<Product>
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
         public ProductRepository(DataDbContext dbContext) : base(dbContext)
         {
